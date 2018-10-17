c99db80fe7b2beb7243fdca1db747f627847339c
212480539 <itzel-silva@hotmail.com>
2018-10-17 17:33:27 -0500

    Fragments > Ventanas

--- app/src/main/java/macias/silva/naye/fragments/segundoFragment.kt
+++ app/src/main/java/macias/silva/naye/fragments/segundoFragment.kt
@@ -1,12 +1,20 @@
 package macias.silva.naye.fragments
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -40,7 +48,40 @@ class segundoFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segundo, container, false)
+        val vistaRaiz = inflater.inflate(R.layout.fragment_segundo,container, false)
+        val web_view = vistaRaiz.findViewById(R.id.webView) as WebView
+        val btnCArgar = vistaRaiz.findViewById(R.id.btnCargar) as Button
+        val progressBar = vistaRaiz.findViewById(R.id.progreso) as ProgressBar
+        val etUrl = vistaRaiz.findViewById(R.id.etDireccion) as EditText
+
+        //obtener y editar ajustes de web view
+        val settings = web_view.settings
+        settings.javaScriptEnabled = true
+        //web cliente
+        web_view.webViewClient = object : WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                Toast.makeText(context,"Se comenzó la carga", Toast.LENGTH_LONG).show()
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                Toast.makeText(context,"${view!!.title}", Toast.LENGTH_LONG).show()
+                super.onPageFinished(view, url)
+            }
+
+        }
+        web_view.webChromeClient = object : WebChromeClient(){
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progressBar.progress = newProgress
+                super.onProgressChanged(view, newProgress)
+            }
+
+        }
+
+        web_view.loadUrl("https://git-scm.com/download/win")
+
+
+        return vistaRaiz
     }
 
     // TODO: Rename method, update argument and hook method into UI event
